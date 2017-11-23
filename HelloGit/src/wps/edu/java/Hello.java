cdadae5f8b08e15e7f1e39adf07b642d3abed997
Wongtu <Wongtu@Wongtu-PC>
2017-11-23 11:08:39 -0600

    Added sum method to hello.java

--- HelloGit/src/wps/edu/java/Hello.java
+++ HelloGit/src/wps/edu/java/Hello.java
@@ -4,6 +4,12 @@ public class Hello {
 
 	
 	public void hello(){
-		System.out.println("Hello This is a change");
+		System.out.println("I have changed test scenario");
 	}
+	
+	public int sum() {
+		return 2;
+	}
+	
+	
 }
